b87c3e1aa0ea0e6b315fc60de52abbff5fc27c6f
KRATIK_BOHRA <kratikbohra5@gmail.com>
2025-02-28 12:32:23 +0530

    github webhook check

--- job_service/db.js
+++ job_service/db.js
@@ -1,16 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//     logging: false, // Disable logging SQL queries in console
-//   }
-// );
-
 require("dotenv").config();
 
 const isTestEnv = process.env.NODE_ENV === "test2";
